f98b6d54c906620d6062fb28895a44ca085601a1
Senger <vsenger@amazon.com>
2024-05-16 15:37:53 -0700

    ok

--- app-fixed/src/main/java/com/example/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.repository;
-
-import com.example.model.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
--- app-fixed/src/main/java/com/example/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.repository;
-
-import com.example.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
